1f24c18862502d5758115d8d76b75c0bdf00fbbb
Uzimonkey123 <normanbabiak@gmail.com>
2024-09-30 21:29:07 +0200

    fix: Fixing key generator class, changing style of returning string

--- KeyGenerator.cpp
+++ KeyGenerator.cpp
@@ -1,15 +1,13 @@
 #include "KeyGenerator.hpp"
 
+string KeyGenerator::combineIpPort(const string& ip, int port) {
+    return ip + ":" + to_string(port);
+}
+
 string KeyGenerator::generateKey(const string& src_ip, int src_port, const string& dst_ip, int dst_port, const string& protocol) {
-    auto buildKey = [](const string& ip_a, int port_a, const string& ip_b, int port_b, const string& protocol) {
-        return ip_a + ":" + to_string(port_a) + "-" + ip_b + ":" + to_string(port_b) + "-" + protocol;
-    };
+    string combinedSrc = combineIpPort(src_ip, src_port);
+    string combinedDst = combineIpPort(dst_ip, dst_port);
 
-    if (src_ip < dst_ip || (src_ip == dst_ip && src_port < dst_port)) {
-        return buildKey(src_ip, src_port, dst_ip, dst_port, protocol);
-
-    } else {
-        return buildKey(dst_ip, dst_port, src_ip, src_port, protocol);
-
-    }
+    return combinedSrc < combinedDst ? combinedSrc + "-" + combinedDst + "-" + protocol 
+                                     : combinedDst + "-" + combinedSrc + "-" + protocol;
 }
--- KeyGenerator.hpp
+++ KeyGenerator.hpp
@@ -6,7 +6,25 @@
 using namespace std;
 
 class KeyGenerator {
+    private:
+        /**
+         * @brief Combines IP and port into a single string
+         * @param ip IP address
+         * @param port Port number
+         * @return Combined string of IP and port
+         */
+        string combineIpPort(const string& ip, int port);
+
     public:
+        /**
+         * @brief Generates a unique key for a connection
+         * @param src_ip Source IP address
+         * @param src_port Source port number
+         * @param dst_ip Destination IP address
+         * @param dst_port Destination port number
+         * @param protocol Protocol used
+         * @return Unique key for the connection
+         */
         string generateKey(const string& src_ip, int src_port, const string& dst_ip, int dst_port, const string& protocol);
 };
 
